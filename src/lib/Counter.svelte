2638e3ce1d70f107384d9ad98932ad88a9a13f35
Amr Elaraby <amrselaraby@gmail.com>
2023-05-31 05:52:31 +0300

    getting started with svelte

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,25 @@
 <script>
-  let count = 0
-  const increment = () => {
-    count += 1
+  export let initialCount;
+  export let maxCount =10;
+  let count = initialCount;
+
+
+
+
+  function increment(){
+    if (count === maxCount) return;
+    count += 1;
   }
+ 
 </script>
 
-<button on:click={increment}>
-  count is {count}
-</button>
+<button on:click={increment}>Clicks {count}</button>
+
+
+
+
+<style>
+  button {
+    padding: 15px 20px;
+  }
+</style>
