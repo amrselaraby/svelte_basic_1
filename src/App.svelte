d38c91a08204f63d2ba59f8565d1645f23568864
Amr Elaraby <amrselaraby@gmail.com>
2023-06-03 14:18:33 +0300

    sec 4: binding components

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,34 @@
 <script>
-	import Button from './lib/Button.svelte'
-	import FaAngellist from 'svelte-icons/fa/FaAngellist.svelte'
-	import FaAllergies from 'svelte-icons/fa/FaAllergies.svelte'
+	import TodoList from './lib/TodoList.svelte'
+	import { v4 as uuid } from 'uuid'
+
+	let todos = [
+		{
+			id: uuid(),
+			title: 'Todo 1',
+			completed: true
+		},
+		{
+			id: uuid(),
+			title: 'Todo 2',
+			completed: false
+		},
+		{
+			id: uuid(),
+			title: 'Todo 3',
+			completed: true
+		}
+	]
+	$: console.log(todos)
 </script>
 
-<Button on:click|once={() => alert(true)} let:isLeftHover size="large" shadow disabled>
-	<div style:width="20px" slot="leftContent">
-		{#if isLeftHover}
-			<FaAngellist />
-		{:else}
-			<FaAllergies />
-		{/if}
-	</div>
-	Some Text
-</Button>
+<h2>{todos.length} Todos</h2>
+<TodoList bind:todos />
+<button
+	on:click={() => {
+		todos = []
+	}}>Update Todos</button
+>
 
 <style>
 </style>
---
+++ src/lib/TodoList.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Button from './Button.svelte'
+	import { v4 as uuid } from 'uuid'
+
+	export let todos = []
+	let inputText = ''
+
+	function handleAddTodo() {
+		if (!inputText) return
+
+		todos = [
+			...todos,
+			{
+				id: uuid(),
+				title: inputText,
+				completed: false
+			}
+		]
+		inputText = ''
+	}
+</script>
+
+<div class="todo-list-wrapper">
+	<ul>
+		{#each todos as { id, title }, index (id)}
+			{@const number = index + 1}
+			<li>{number}- {title}</li>
+		{/each}
+	</ul>
+	{inputText.length}/140
+	<form class="add-todo-form" on:submit|preventDefault={handleAddTodo}>
+		<input bind:value={inputText} />
+		<Button type="submit" disabled={!inputText}>Add</Button>
+	</form>
+</div>
