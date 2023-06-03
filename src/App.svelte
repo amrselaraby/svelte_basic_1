49ff9e50856bb46453c7f9c5091888a5113b176b
Amr Elaraby <amrselaraby@gmail.com>
2023-06-03 12:16:03 +0300

    finished section 3

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,19 @@
 <script>
- import Counter from './lib/Counter.svelte'
- const props = {
-  maxCount: 10,
-  initialCount: 5 
- };
-
- console.log($$props, $$restProps)
+	import Button from './lib/Button.svelte'
+	import FaAngellist from 'svelte-icons/fa/FaAngellist.svelte'
+	import FaAllergies from 'svelte-icons/fa/FaAllergies.svelte'
 </script>
 
-<Counter {...props}/>
+<Button on:click|once={() => alert(true)} let:isLeftHover size="large" shadow disabled>
+	<div style:width="20px" slot="leftContent">
+		{#if isLeftHover}
+			<FaAngellist />
+		{:else}
+			<FaAllergies />
+		{/if}
+	</div>
+	Some Text
+</Button>
 
 <style>
-  
 </style>
